d49002cdfdc0c830c84a21e2d906a8a866c7de7e
rdwangyu <rdwangyu@foxmail.com>
2025-04-07 17:41:34 +0800

    add package 'lsp'

--- init.el
+++ init.el
@@ -22,6 +22,26 @@
 (setq c-default-style "linux"
       c-basic-offset 4)
 
+(use-package lsp-mode
+  :ensure t
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook
+  ((c++-mode . lsp)
+   (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+
+(use-package lsp-ui
+  :ensure t
+  :commands lsp-ui-mode)
+
+(use-package clang-format
+  :ensure t
+  :hook (c++-mode . (lambda ()
+                      (local-set-key (kbd "C-c f") 'clang-format-buffer))))
+(setq clang-format-style-option "file")
+
+
 (use-package ace-window
   :ensure t)
 (global-set-key (kbd "M-o") 'ace-window)
@@ -40,19 +60,4 @@
 (set-frame-font "Monaco 10" nil t)
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 (provide 'init)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("4c56af497ddf0e30f65a7232a8ee21b3d62a8c332c6b268c81e9ea99b11da0d3" "524fa911b70d6b94d71585c9f0c5966fe85fb3a9ddd635362bfabd1a7981a307" default))
- '(package-selected-packages
-   '(org-pomodoro buffer-move solarized-theme popwin melancholy-theme company ace-window)))
